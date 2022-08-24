d391f266eedf09db595b5168d48bbdc23009a04c
thealamenthed <dalila.boualemfr@gmail.com>
2022-08-24 17:05:35 +0200

    update bdd add dump sql

--- parts/footer.php
+++ parts/footer.php
@@ -2,5 +2,6 @@
 <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
 <script src="https://unpkg.com/@barba/core"></script>
 <script src="./assets/js/main.js"></script>
+<script src="./assets/js/favorites.js"></script>
 </body>
 </html>
